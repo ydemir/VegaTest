fb49e0d23657b48b0124befcbe424ee3c6ae9743
Yusuf Demir <yusuf@bilgiedin.net>
2017-05-07 03:26:31 +0300

    Domain Modellerimiz oluşturuldu Entity Framework Core ayalarımız yapıldı.

---
+++ Models/Marka.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace AracTescil.Models
+{
+    public class Marka
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+
+        public ICollection<Model> Modeller { get; set; }
+
+        //Marka.Modeller=new Collection<Modeller>();
+
+        public Marka()
+        {
+            Modeller = new Collection<Model>();
+        }
+         
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,13 @@
+﻿namespace AracTescil.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+
+        public Marka Marka { get; set; }
+        public int MarkaId { get; set; }
+
+        
+    }
+}
